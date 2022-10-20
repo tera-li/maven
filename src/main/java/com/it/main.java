155a36b1423d9e002ef4692f0165362fa6c434fc
surprise <776745393@qq.com>
2022-10-20 15:42:10 +0800

    feat(query-search): mapper

--- src/main/java/com/it/main.java
+++ src/main/java/com/it/main.java
@@ -9,10 +9,18 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class main {
     public static void main(String[] args) throws IOException {
+        // 实体类封装参数
+        User user = new User();
+        user.setRunoob_id(2);
+        // map集合封住鞥
+        Map map = new HashMap();
+        map.put("runoob_id", 2);
         // 记载核心配置
         InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
@@ -22,9 +30,17 @@ public class main {
         // 获取mapper接口的代理对象
         UserMapper users = session.getMapper(UserMapper.class);
         // 实现代理接口
-        List<User> userList = users.selectOne(2);
+        List<User> userList1 = users.selectAll();
+        List<User> userList2 = users.selectAllList();
+        List<User> userList3 = users.selectOne(2);
+        List<User> userList4 = users.selectCondition(user);
+        List<User> userList5 = users.selectConditionMap(map);
         System.out.println("查到了");
-        System.out.println(userList);
+        System.out.println(userList1);
+        System.out.println(userList2);
+        System.out.println(userList3);
+        System.out.println(userList4);
+        System.out.println(userList5);
         session.close();
     }
 }
--- src/main/java/com/it/mapper/UserMapper.java
+++ src/main/java/com/it/mapper/UserMapper.java
@@ -3,9 +3,12 @@ package com.it.mapper;
 import com.it.user.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
     List<User> selectAll();
     List<User> selectAllList();
     List<User> selectOne(int id);
+    List<User> selectCondition(User user);
+    List<User> selectConditionMap(Map map);
 }
