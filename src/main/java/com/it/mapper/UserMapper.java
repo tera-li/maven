417077d2cf7f2401a44719d2a4f83bb160b1429c
surprise <776745393@qq.com>
2022-10-26 17:18:27 +0800

    feat(mapper): param

--- src/main/java/com/it/mapper/UserMapper.java
+++ src/main/java/com/it/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.it.mapper;
 
 import com.it.user.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
@@ -9,7 +10,7 @@ public interface UserMapper {
     List<User> selectAll();
     List<User> selectAllList();
     List<User> selectOne(int id);
-    List<User> selectCondition(User user);
+    List<User> selectCondition(@Param("user") User user);
     List<User> selectConditionMap(Map map);
     List<User> selectConditionChoose(Map map);
     boolean addOne(User user);
